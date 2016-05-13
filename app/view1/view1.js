f24dc0781a06896b9875005a0f2245c8255c1311
Jon Vinson <jvinson@nctcog.org>
2016-05-13 14:18:35 -0500

    add basic ui grid

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+angular.module('myApp.view1', ['ngRoute', 'ngTouch', 'ui.grid'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
@@ -11,9 +11,15 @@ angular.module('myApp.view1', ['ngRoute'])
 
 .controller('View1Ctrl', ['$scope', '$http',function($scope, $http) {
   $scope.model = {};
+  $scope.model.SelectedProgram = {};
 
   $http.get('data/test.json').success(function(data) {
     $scope.model = data;
-    //console.log('data = ' + data);
+  //  $scope.SelectedProgram = data.SelectedProgram;
+    console.log('data = ' + data);
   });
+
+  $scope.selectProgram = function (program) {
+    $scope.model.SelectedProgram = program;
+  };
 }]);
